3f718989149a3a4e3181ca2ec9cec1f8d02d45a4
VladZakharko <zakvla000@gmail.com>
2023-11-15 21:41:32 +0200

    jj

---
+++ main.js
@@ -0,0 +1,11 @@
+// let str = `Creator It Academy`;
+
+// console.log(str.match(/creator/gi));
+
+// let matches = `JavaScript`.match(/HTMl/) || [];
+
+// if(!matches.length) {
+//     console.log(`Співпадіння немає`)
+// }
+
+console.log(`We will, we will`.replace(/we/i, `I`));
